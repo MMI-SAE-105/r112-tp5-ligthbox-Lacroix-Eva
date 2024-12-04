c69943ddb082f6a50057dc70d84e4ddf01b86831
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2024-12-04 14:39:06 +0100

    Sélection de LightBox et géstion de sa fermeture

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 // *** LIGHTBOX ***
+// Étape 1 : Sélectionner le LightBox et gérer sa fermeture
+const lb = document.querySelector("#lightbox");
+
+lb.addEventListener("click", function() {
+    lb.close(); // Fermer le LightBox
+});
